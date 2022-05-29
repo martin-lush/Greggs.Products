3e3eb4845cd651d6c9c28fee2521f216755dd1e1
Martin Lush <martin-hornsby@ntlworld.com>
2022-05-29 14:49:21 +0100

    User story 1

---
+++ Greggs.Products.UnitTests/ProductControllerTests.cs.cs
@@ -0,0 +1,49 @@
+using Greggs.Products.Api.Controllers;
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Greggs.Products.UnitTests;
+
+[TestFixture]
+public class ProductControllerTests
+{
+    private IEnumerable<Product> _productDatabase;
+    private Mock<IDataAccess<Product>> _dataAccess;
+    private ProductController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _productDatabase = new List<Product>()
+        {
+            new() { Name = "Sausage Roll", PriceInPounds = 1m },
+            new() { Name = "Vegan Sausage Roll", PriceInPounds = 1.1m },
+            new() { Name = "Steak Bake", PriceInPounds = 1.2m },
+        };
+
+        _dataAccess = new Mock<IDataAccess<Product>>();
+        _controller = new ProductController(Mock.Of<ILogger<ProductController>>(), _dataAccess.Object);
+
+        _dataAccess.Setup(x => x.List(It.IsAny<int>(), It.IsAny<int>())).Returns(_productDatabase);
+    }
+
+    [Test]
+    public void Uses_data_access_to_get_products()
+    {
+        var result = _controller.Get();
+
+        _dataAccess.Verify(x => x.List(It.IsAny<int>(), It.IsAny<int>()), Times.Once());
+    }
+
+    [Test]
+    public void Returns_expected_products()
+    {
+        var result = _controller.Get();
+
+        CollectionAssert.AreEquivalent(_productDatabase, result);
+    }
+}
